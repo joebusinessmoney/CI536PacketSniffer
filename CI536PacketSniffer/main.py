306dddca2cd6a2663d1389e5f4ea790ba089b5c8
joebusinessmoney <joejp103@gmail.com>
2024-05-01 21:38:38 +0100

    more UI improvements, primarly to the packet info window

---
+++ CI536PacketSniffer/main.py
@@ -0,0 +1,87 @@
+import platform
+import time
+import subprocess
+import sys
+import os
+
+class Main():
+    _banner = ('''
+   _____  __                __   _                  ____                __          __     _____         _  ____ ____           
+  / ___/ / /_ ____ _ _____ / /_ (_)____   ____ _   / __ \ ____ _ _____ / /__ ___   / /_   / ___/ ____   (_)/ __// __/___   _____
+  \__ \ / __// __ `// ___// __// // __ \ / __ `/  / /_/ // __ `// ___// //_// _ \ / __/   \__ \ / __ \ / // /_ / /_ / _ \ / ___/
+ ___/ // /_ / /_/ // /   / /_ / // / / // /_/ /  / ____// /_/ // /__ / ,<  /  __// /_    ___/ // / / // // __// __//  __// /    
+/____/ \__/ \__,_//_/    \__//_//_/ /_/ \__, /  /_/     \__,_/ \___//_/|_| \___/ \__/   /____//_/ /_//_//_/  /_/   \___//_/     
+                                       /____/       
+   ''')
+
+    os = platform.system()
+
+    def installScapy(self, package, action):
+        try:
+            command = [sys.executable, "-m", "pip", "install", action, package]
+
+            if self.os == "Linux":
+                command.insert(4, "--target")
+                command.insert(5, "/usr/lib/python3/dist-packages")
+            subprocess.check_call(command)
+
+            if action == "--upgrade":
+                print("*** Scapy Successfully Upgaded, System Will Now Reboot... ***")
+
+                time.sleep(3)
+
+                python_executable = sys.executable
+                os.execl(python_executable, python_executable, *sys.argv)
+            else:
+                print("*** Scapy is Installed, Initialising Packet Sniffer ***")
+                return True
+        except Exception as error:
+            print(error)
+            return False
+        
+
+    def checkDependencies(self):
+        try:
+            import scapy
+            if scapy.__version__ != "2.5.0":
+                print("*** Older Version of Scapy is Installed, Installing Upgraded Version... ***")
+
+                time.sleep(3)
+                
+                self.installScapy("scapy", "--upgrade")
+            else:
+                print("*** Scapy is Installed, Initialising Packet Sniffer ***")
+                return True
+        except ImportError:
+            install = input("*** Scapy is Not Installed on This Device. This Dependency is Required for This Program. Would You Like to Install it? (Y/n) ***")
+
+            if install.lower() in ["y", "yes"]:
+                installed = self.installScapy("scapy", "install")
+                return installed
+            else:
+                return False
+
+    def startUp(self):
+        print(self._banner)
+        print("*** Detecting Host Operating System ... ***")
+        time.sleep(3)
+
+        print("*** Host Operating System Detected: " + self.os + " ***")
+
+        print("*** Checking if Dependency 'Scapy' is Installed ... ***")
+        time.sleep(3)
+
+        complete = self.checkDependencies()
+
+        if complete:
+            time.sleep(3)
+            
+            from mvc import MVC
+            mvc = MVC(self.os)
+            mvc.mainloop()
+        else:
+            print("*** Exiting Program ***")
+            quit()
+
+main = Main()
+main.startUp()
--- view.py
+++ view.py
@@ -39,7 +39,148 @@ class View(ctk.CTkFrame):
         for index, iface_id in enumerate(self.interfaces):
             iface = self.interfaces[iface_id]
             description = iface.description if hasattr(iface, 'description') else iface.name
-            ctk.CTkRadioButton(self, text=description, variable=self.interface_var, value=iface.name, fg_color="#20C20E", hover_color="#16880A", text_color="white", border_color="white").grid(row=index + 2, column=0, columnspan=2, sticky='w', padx=(10))
+            ctk.CTkRadioButton(self, text=description, variable=self.interface_var, value=iface.name, fg_color="#20C20E", hover_color="#16880A", text_color="white", border_color="white").grid(row=index + 2, column=0, columnspan=2, sticky='w', padx=(10), pady=(5))
+        
+
+        # Sniff button
+        self.sniff_button = ctk.CTkButton(self, text='Sniff', command=self.sniffButtonClicked)
+        View.green_button_style(self.sniff_button)
+        self.sniff_button.grid(row=len(self.interfaces) + 2, column=0, columnspan=2, pady=10)
+
+        # Message label
+        self.message_label = ctk.CTkLabel(self, text='')
+        self.message_label.grid(row=len(self.interfaces) + 3, column=0, columnspan=2, pady=5)
+
+    def setController(self, controller):
+        self.controller = controller
+
+    def sniffButtonClicked(self):
+        selected_interface = self.interface_var.get()
+        if selected_interface:
+            self.controller.setInterface(selected_interface)
+        else:
+            self.showError("Please select an interface.")
+
+    def showError(self, message):
+        self.message_label['text'] = message
+        self.after(3000, self.hideMessage)
+
+    def hideMessage(self):
+        self.message_label['text'] = ''
+
+    def updateUI(self):
+        self.removeInterface()
+        self.createPacketListBox()
+
+    def removeInterface(self):
+        for widget in self.winfo_children():
+            widget.grid_remove()
+
+    def createPacketListBox(self):
+        self.packets_listbox = tk.Listbox(self, width=100, font=('sans_serif', 18), borderwidth=0, highlightthickness=0, bg="#404040", fg="white", selectbackground="#16880A", selectforeground="white")
+        self.packets_listbox.grid(row=0, column=0, columnspan=2, pady=(10, 5), padx=10, sticky='w')
+        self.packets_listbox.bind('<<ListboxSelect>>', self.showPacketInfo)
+
+        self.sniffing_button = ctk.CTkButton(self, text='Stop', command=self.stopSniffing)
+        View.red_button_style(self.sniffing_button)
+        self.sniffing_button.grid(row = 1, column=0, columnspan=2, pady=10)
+
+    def stopSniffing(self):
+        self.controller.stopSniffing()
+    
+    def startSniffing(self):
+        self.controller.startSniffing()
+
+    def updateButton(self, action):
+        if action == "Start":
+            self.sniffing_button.configure(text="Start", command=self.startSniffing)
+            View.green_button_style(self.sniffing_button)
+        else:
+            self.sniffing_button.configure(text="Stop", command=self.stopSniffing)
+            View.red_button_style(self.sniffing_button)
+
+    def updatePackets(self, packet):
+        packet_info = self.formatPacketInfo(packet)
+        self.packets_listbox.insert(tk.END, packet_info)
+        self.packets_listbox.yview_moveto(1.0)
+
+    def formatPacketInfo(self, packet):
+        if packet.ip:
+            src_ip = packet.ip.src_ip
+            dst_ip = packet.ip.dst_ip
+            protocol_ip = packet.ip.proto
+            protocol = "Unknown"
+
+            if protocol_ip == 6:
+                protocol = "TCP"
+            elif protocol_ip == 17:
+                protocol = "UDP"
+            elif protocol_ip == 1:
+                protocol = "ICMP"
+
+            return f"Source IP: {src_ip}, Destination IP: {dst_ip}, Protocol: {protocol}"
+        elif packet.ether:
+            src_mac = packet.ether.src_mac
+            dst_mac = packet.ether.dst_mac
+            ether_type = packet.ether.ether_type
+            protocol = "Unknown"
+
+            if ether_type == 2048:
+                protocol = "IPv4"
+            elif ether_type == 2054:
+                protocol = "ARP"
+                if dst_mac == "ff:ff:ff:ff:ff:ff":
+                    dst_mac = "BROADCAST"
+            elif ether_type == 34525:
+                protocol = "IPv6"
+
+            return f"Source MAC: {src_mac}, Destination MAC: {dst_mac}, Protocol: {protocol}"
+        else:
+            return "No Information Available"
+        
+
+import tkinter as tk
+from tkinter import ttk
+import scapy.all as scapy
+import customtkinter as ctk
+from Packet import Packet
+
+class View(ctk.CTkFrame):
+    def __init__(self, parent, os):
+        super().__init__(parent)
+        self.os = os
+        self.controller = None
+        self.packets_listbox = None
+        self.interface_var = tk.StringVar(value="")
+        self.interfaces = scapy.ifaces
+
+        self.setupUI()
+    
+    # Styling elements
+
+    def green_button_style(widget):
+        widget.configure(fg_color="#20C20E", hover_color="#16880A")
+
+    def red_button_style(widget):
+        widget.configure(fg_color="red", hover_color="darkred")        
+
+    def application_style(master):
+        master.configure(fg_color="#404040", border_color="#404040")
+
+
+    def setupUI(self):
+
+        View.application_style(self)        
+
+        # Labels
+        self.title = ctk.CTkLabel(self, text='List of Currently Detected Interfaces', text_color="white").grid(row=0, column=0, columnspan=2, pady=(10, 5), sticky='w', padx=(5))
+        self.subtitle = ctk.CTkLabel(self, text='Please select an interface to sniff from:', text_color="white").grid(row=1, column=0, columnspan=2, pady=(5, 10), sticky='w', padx=(10))
+
+        # Interface Radiobuttons
+        for index, iface_id in enumerate(self.interfaces):
+            iface = self.interfaces[iface_id]
+            description = iface.description if hasattr(iface, 'description') else iface.name
+            ctk.CTkRadioButton(self, text=description, variable=self.interface_var, value=iface.name, fg_color="#20C20E", hover_color="#16880A", text_color="white", border_color="white").grid(row=index + 2, column=0, columnspan=2, sticky='w', padx=(10), pady=(5))
         
 
         # Sniff button
@@ -144,25 +285,46 @@ class View(ctk.CTkFrame):
         if selected_index:
             packet_window = tk.Toplevel(self, background="#404040")
             packet_window.title("Packet Information")
-            
+            packet_window.geometry("450x600") 
+
+            # Create a frame to contain the packet information
+            frame = tk.Frame(packet_window, background="#404040")
+            frame.pack(fill=tk.BOTH, expand=True)
+
+            # Create a canvas to enable scrolling
+            canvas = tk.Canvas(frame, background="#404040")
+            canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+            # Add a scrollbar to the canvas
+            scrollbar = tk.Scrollbar(frame, orient=tk.VERTICAL, command=canvas.yview, background="#404040")
+            scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+            canvas.config(yscrollcommand=scrollbar.set)
+
+            # Create another frame to hold the actual packet information labels
+            inner_frame = tk.Frame(canvas, background="#404040")  
+            canvas.create_window((0, 0), window=inner_frame, anchor=tk.NW)
             packet_info = self.controller.model.packets[selected_index[0]]
 
-            try:
+            # Styling elements for packet info labels
+            label_style = {'background': '#404040', 'foreground': 'white', 'anchor': 'w', 'font': ('Helvetica', 16)}
+            title_style = {'background': '#404040', 'foreground': 'white', 'font': 'bold, 20', 'relief': 'raised'}
+            raw_style = {'background': '#404040', 'foreground': 'white', 'font': ('sans-serif', 12)}
 
+            try:
                 if packet_info.ether:
                     src_mac = packet_info.ether.src_mac
                     dst_mac = packet_info.ether.dst_mac
                     type_mac = packet_info.ether.ether_type
 
-                    packet_ether = tk.Label(packet_window, text="Ethernet:")
-                    packet_ether_src = tk.Label(packet_window, text="Source MAC:" + str(src_mac))
-                    packet_ether_dst = tk.Label(packet_window, text="Destination MAC:" + str(dst_mac))
-                    packet_ether_type = tk.Label(packet_window, text="Ether Type:" + str(type_mac))
+                    packet_ether = tk.Label(inner_frame, text="Ethernet:", **title_style)
+                    packet_ether_src = tk.Label(inner_frame, text="Source MAC: " + str(src_mac), **label_style)
+                    packet_ether_dst = tk.Label(inner_frame, text="Destination MAC: " + str(dst_mac), **label_style)
+                    packet_ether_type = tk.Label(inner_frame, text="Ether Type: " + str(type_mac), **label_style)
 
                     packet_ether.pack()
-                    packet_ether_src.pack()
-                    packet_ether_dst.pack()
-                    packet_ether_type.pack()
+                    packet_ether_src.pack(fill='x')
+                    packet_ether_dst.pack(fill='x')
+                    packet_ether_type.pack(fill='x')
             
                 if packet_info.ip:
                     src_ip = packet_info.ip.src_ip
@@ -173,23 +335,23 @@ class View(ctk.CTkFrame):
                     flags_ip = packet_info.ip.flags
                     id_ip = packet_info.ip.id
 
-                    packet_ip = tk.Label(packet_window, text="IP:")
-                    packet_ip_src = tk.Label(packet_window, text="Source IP:" + str(src_ip))
-                    packet_ip_dst = tk.Label(packet_window, text="Destination IP:" + str(dst_ip))
-                    packet_ip_proto = tk.Label(packet_window, text="Protocol:" + str(proto_ip))
-                    packet_ip_tos = tk.Label(packet_window, text="Type of Service:" + str(tos_ip))
-                    packet_ip_ttl = tk.Label(packet_window, text="Time to Live:" + str(ttl_ip))
-                    packet_ip_flags = tk.Label(packet_window, text="Set Flags:" + str(flags_ip))
-                    packet_ip_id = tk.Label(packet_window, text="ID:" + str(id_ip))
+                    packet_ip = tk.Label(inner_frame, text="IP:", **title_style)
+                    packet_ip_src = tk.Label(inner_frame, text="Source IP: " + str(src_ip), **label_style)
+                    packet_ip_dst = tk.Label(inner_frame, text="Destination IP: " + str(dst_ip), **label_style)
+                    packet_ip_proto = tk.Label(inner_frame, text="Protocol: " + str(proto_ip), **label_style)
+                    packet_ip_tos = tk.Label(inner_frame, text="Type of Service: " + str(tos_ip), **label_style)
+                    packet_ip_ttl = tk.Label(inner_frame, text="Time to Live: " + str(ttl_ip), **label_style)
+                    packet_ip_flags = tk.Label(inner_frame, text="Set Flags: " + str(flags_ip), **label_style)
+                    packet_ip_id = tk.Label(inner_frame, text="ID: " + str(id_ip), **label_style)
 
                     packet_ip.pack()
-                    packet_ip_src.pack()
-                    packet_ip_dst.pack()
-                    packet_ip_proto.pack()
-                    packet_ip_tos.pack()
-                    packet_ip_ttl.pack()
-                    packet_ip_flags.pack()
-                    packet_ip_id.pack()
+                    packet_ip_src.pack(fill='x')
+                    packet_ip_dst.pack(fill='x')
+                    packet_ip_proto.pack(fill='x')
+                    packet_ip_tos.pack(fill='x')
+                    packet_ip_ttl.pack(fill='x')
+                    packet_ip_flags.pack(fill='x')
+                    packet_ip_id.pack(fill='x')
             
                 if packet_info.tcp:
                     src_tcp = packet_info.tcp.src_port
@@ -199,21 +361,21 @@ class View(ctk.CTkFrame):
                     flags_tcp = packet_info.tcp.flags
                     window_tcp = packet_info.tcp.window
 
-                    packet_tcp = tk.Label(packet_window, text="TCP:")
-                    packet_tcp_src = tk.Label(packet_window, text="Source Port:" + str(src_tcp))
-                    packet_tcp_dst = tk.Label(packet_window, text="Destination Port:" + str(dst_tcp))
-                    packet_tcp_seq = tk.Label(packet_window, text="Sequence:" + str(seq_tcp))
-                    packet_tcp_ack = tk.Label(packet_window, text="Acknowledge:" + str(ack_tcp))
-                    packet_tcp_flags = tk.Label(packet_window, text="Flags:" + str(flags_tcp))
-                    packet_tcp_window = tk.Label(packet_window, text="Window:" + str(window_tcp))
+                    packet_tcp = tk.Label(inner_frame, text="TCP:", **title_style)
+                    packet_tcp_src = tk.Label(inner_frame, text="Source Port: " + str(src_tcp), **label_style)
+                    packet_tcp_dst = tk.Label(inner_frame, text="Destination Port: " + str(dst_tcp), **label_style)
+                    packet_tcp_seq = tk.Label(inner_frame, text="Sequence: " + str(seq_tcp), **label_style)
+                    packet_tcp_ack = tk.Label(inner_frame, text="Acknowledge: " + str(ack_tcp), **label_style)
+                    packet_tcp_flags = tk.Label(inner_frame, text="Flags: " + str(flags_tcp), **label_style)
+                    packet_tcp_window = tk.Label(inner_frame, text="Window: " + str(window_tcp), **label_style)
 
                     packet_tcp.pack()
-                    packet_tcp_src.pack()
-                    packet_tcp_dst.pack()
-                    packet_tcp_seq.pack()
-                    packet_tcp_ack.pack()
-                    packet_tcp_flags.pack()
-                    packet_tcp_window.pack()
+                    packet_tcp_src.pack(fill='x')
+                    packet_tcp_dst.pack(fill='x')
+                    packet_tcp_seq.pack(fill='x')
+                    packet_tcp_ack.pack(fill='x')
+                    packet_tcp_flags.pack(fill='x')
+                    packet_tcp_window.pack(fill='x')
 
                 if packet_info.udp:
                     src_udp = packet_info.udp.src_port
@@ -221,17 +383,17 @@ class View(ctk.CTkFrame):
                     len_udp = packet_info.udp.len
                     checksum_udp = packet_info.udp.checksum
 
-                    packet_udp = tk.Label(packet_window, text="UDP:")
-                    packet_udp_src = tk.Label(packet_window, text="Source Port:" + str(src_udp))
-                    packet_udp_dst = tk.Label(packet_window, text="Destination Port:" + str(dst_udp))
-                    packet_udp_len = tk.Label(packet_window, text="Length:" + str(len_udp))
-                    packet_udp_checksum = tk.Label(packet_window, text="Checksum:" + str(checksum_udp))
+                    packet_udp = tk.Label(inner_frame, text="UDP: ", **title_style)
+                    packet_udp_src = tk.Label(inner_frame, text="Source Port: " + str(src_udp), **label_style)
+                    packet_udp_dst = tk.Label(inner_frame, text="Destination Port: " + str(dst_udp), **label_style)
+                    packet_udp_len = tk.Label(inner_frame, text="Length: " + str(len_udp), **label_style)
+                    packet_udp_checksum = tk.Label(inner_frame, text="Checksum: " + str(checksum_udp), **label_style)
 
                     packet_udp.pack()
-                    packet_udp_src.pack()
-                    packet_udp_dst.pack()
-                    packet_udp_len.pack()
-                    packet_udp_checksum.pack()
+                    packet_udp_src.pack(fill='x')
+                    packet_udp_dst.pack(fill='x')
+                    packet_udp_len.pack(fill='x')
+                    packet_udp_checksum.pack(fill='x')
 
                 if packet_info.icmp:
                     type_icmp = packet_info.icmp.type
@@ -239,31 +401,37 @@ class View(ctk.CTkFrame):
                     id_icmp = packet_info.icmp.id
                     seq_icmp = packet_info.icmp.seq
 
-                    packet_icmp = tk.Label(packet_window, text="ICMP:")
-                    packet_icmp_type = tk.Label(packet_window, text="Type:" + str(type_icmp))
-                    packet_icmp_code = tk.Label(packet_window, text="Code:" + str(code_icmp))
-                    packet_icmp_id = tk.Label(packet_window, text="ID:" + str(id_icmp))
-                    packet_icmp_seq = tk.Label(packet_window, text="Sequence:" + str(seq_icmp))
+                    packet_icmp = tk.Label(inner_frame, text="ICMP: ", **title_style)
+                    packet_icmp_type = tk.Label(inner_frame, text="Type: " + str(type_icmp), **label_style)
+                    packet_icmp_code = tk.Label(inner_frame, text="Code: " + str(code_icmp), **label_style)
+                    packet_icmp_id = tk.Label(inner_frame, text="ID: " + str(id_icmp), **label_style)
+                    packet_icmp_seq = tk.Label(inner_frame, text="Sequence: " + str(seq_icmp), **label_style)
 
                     packet_icmp.pack()
-                    packet_icmp_type.pack()
-                    packet_icmp_code.pack()
-                    packet_icmp_id.pack()
-                    packet_icmp_seq.pack()
+                    packet_icmp_type.pack(fill='x')
+                    packet_icmp_code.pack(fill='x')
+                    packet_icmp_id.pack(fill='x')
+                    packet_icmp_seq.pack(fill='x')
 
                 if packet_info.raw:
                     raw_data = packet_info.raw.load
 
-                    packet_raw = tk.Label(packet_window, text="Raw:")
+                    packet_raw = tk.Label(inner_frame, text="Raw:", **title_style)
                     packet_raw.pack()
 
                     
-                    chunk_size = 16
+                    chunk_size = 19
                     for i in range(0, len(raw_data), chunk_size):
                         chunk = raw_data[i:i + chunk_size]
                         chunk_hex = ' '.join(f"{byte:02x}" for byte in chunk)
-                        tk.Label(packet_window, text=chunk_hex).pack()
+                        tk.Label(inner_frame, text=chunk_hex, **raw_style).pack()
 
-            except:
-                packet_none = tk.Label(packet_window, text="Information not Available")
+            except Exception as e:
+                packet_none = tk.Label(inner_frame, text="Information not Available", **label_style)
                 packet_none.pack()
+
+        # Bind the canvas to the mouse scroll event
+        canvas.bind_all("<MouseWheel>", lambda event: canvas.yview_scroll(int(-1*(event.delta/120)), "units"))
+
+        # Configure canvas to update scroll region
+        inner_frame.bind("<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
